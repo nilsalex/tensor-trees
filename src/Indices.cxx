17941d0c22cdf78dcb88bf1b1aca34a9f87a61cf
Nils Alex <nils@famalex.de>
2018-05-16 13:07:40 +0200

    Added Indices.cxx

---
+++ src/Indices.cxx
@@ -0,0 +1,94 @@
+#include "Indices.hxx"
+
+bool nextIndexPair (std::pair<char, char> & indices) {
+  if (indices.second < 3) {
+    indices.second++;
+    return true;
+  } else if (indices.first < 3) {
+    indices.first++;
+    indices.second = 0;
+    return true;
+  } else {
+    indices = {0, 0};
+    return false;
+  }
+}
+
+bool nextIndexPairSymmetric (std::pair<char, char> & indices) {
+  assert (indices.first <= indices.second);
+  if (indices.second < 3) {
+    indices.second++;
+    assert (indices.first <= indices.second);
+    return true;
+  } else if (indices.first != indices.second) {
+    indices.first++;
+    indices.second = indices.first;
+    assert (indices.first <= indices.second);
+    return true;
+  } else {
+    indices = {0, 0};
+    assert (indices.first <= indices.second);
+    return false;
+  }
+}
+
+bool nextIndexPairAntiSymmetric (std::pair<char, char> & indices) {
+  assert (indices.first < indices.second);
+  if (indices.second < 3) {
+    ++indices.second;
+    assert (indices.first < indices.second);
+    return true;
+  } else if (indices.second > indices.first + 1) {
+    ++indices.first;
+    indices.second = indices.first + 1;
+    assert (indices.first < indices.second);
+    return true;
+  } else {
+    indices = {0, 1};
+    return false;
+  }
+}
+
+bool nextAreaIndices (std::pair<std::pair<char, char>, std::pair<char, char>> & indices) {
+  assert (indices.first <= indices.second);
+  assert (indices.first.first < indices.first.second);
+  assert (indices.second.first < indices.second.second);
+  if (indices.second < std::pair<char, char> {2, 3}) {
+    nextIndexPairAntiSymmetric (indices.second);
+    assert (indices.first <= indices.second);
+    assert (indices.first.first < indices.first.second);
+    assert (indices.second.first < indices.second.second);
+    return true;
+  } else if (indices.first != indices.second) {
+    nextIndexPairAntiSymmetric (indices.first);
+    indices.second = indices.first;
+    assert (indices.first <= indices.second);
+    assert (indices.first.first < indices.first.second);
+    assert (indices.second.first < indices.second.second);
+    return true;
+  } else {
+    indices = {{0, 1}, {0, 1}};
+    assert (indices.first <= indices.second);
+    assert (indices.first.first < indices.first.second);
+    assert (indices.second.first < indices.second.second);
+    return false;
+  }
+}
+
+bool nextAreaIndicesPairSymmetric (std::pair<std::pair<std::pair<char, char>, std::pair<char, char>>, std::pair<std::pair<char, char>, std::pair<char, char>>> & indices) {
+  assert (indices.first <= indices.second);
+  if (indices.second < std::pair<std::pair<char, char>, std::pair<char, char>> {{2, 3}, {2, 3}}) {
+    nextAreaIndices (indices.second);
+    assert (indices.first <= indices.second);
+    return true;
+  } else if (indices.first != indices.second) {
+    nextAreaIndices (indices.first);
+    indices.second = indices.first;
+    assert (indices.first <= indices.second);
+    return true;
+  } else {
+    indices = {{{0, 1}, {0, 1}}, {{0, 1}, {0, 1}}};
+    assert (indices.first <= indices.second);
+    return false;
+  }
+}
